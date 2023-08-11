bf959eba5128ad6f172dbc945adc331ebf9be116
Armin Rauschenberger <armin.rauschenberger@uni.lu>
2023-08-11 21:43:48 +0000

    version 0.0.9

---
+++ inst/doc/article.R
@@ -0,0 +1,3 @@
+## ----echo=FALSE---------------------------------------------------------------
+knitr::include_url("https://publications.uni.lu/bitstream/10993/55464/2/Predicting%20dichotomised%20outcomes%20from%20high%20dimensional%20data%20in%20biomedicine.pdf")
+
